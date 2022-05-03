e230e52d1cfcf956db2c53e428196469014be281
ChangSuChoi <choics2623@gmail.com>
2022-05-03 23:55:04 +0900

    readFile

---
+++ progressRawFIle/app/src/main/java/com/example/progressrawfile/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.progressrawfile;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPrev, btnNext;
+    myPictureView myPicture;
+    int curNum = 0;
+    File[] imageFiles;
+    String imageFname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 이미지 뷰어");
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        myPicture = (myPictureView) findViewById(R.id.myPictureView1);
+
+        imageFiles = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/Pictures").listFiles();
+
+        imageFname = imageFiles[curNum].toString();
+        myPicture.imagePath = imageFname;
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (curNum <= 0) {
+                    Toast.makeText(getApplicationContext(), "첫번째 그림입니다", Toast.LENGTH_SHORT).show();
+                } else {
+                    curNum--;
+                    imageFname = imageFiles[curNum].toString();
+                    myPicture.imagePath = imageFname;
+                    myPicture.invalidate();
+                }
+            }
+        });
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (curNum >= imageFiles.length) {
+                    Toast.makeText(getApplicationContext(), "첫번째 그림입니다", Toast.LENGTH_SHORT).show();
+                } else {
+                    curNum++;
+                    imageFname = imageFiles[curNum].toString();
+                    myPicture.imagePath = imageFname;
+                    myPicture.invalidate();
+                }
+            }
+        });
+    }
+
+}
+
